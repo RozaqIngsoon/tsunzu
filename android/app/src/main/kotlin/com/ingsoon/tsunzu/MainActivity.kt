85e9a6801eac2b16f83ec51a874662487d760081
Rozaq Ingsoon <email.ingsoon@gmail.com>
2023-09-26 15:33:06 +0700

    first tsun zu

---
+++ android/app/src/main/kotlin/com/ingsoon/tsunzu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ingsoon.tsunzu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen.dart
@@ -0,0 +1,40 @@
+
+import 'package:flutter/material.dart';
+import 'package:page_flip/page_flip.dart';
+import 'package:tsunzu/page.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final _controller = GlobalKey<PageFlipWidgetState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageFlipWidget(
+        key: _controller,
+        backgroundColor: Colors.white,
+        lastPage: Container(
+            color: Colors.white,
+            child: const Center(child: Text('Last Page!'))),
+        children: <Widget>[
+          for (var i = 0; i < 10; i++) DemoPage(page: i),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.looks_4_outlined),
+        onPressed: () {
+          _controller.currentState?.goToPage(4);
+          setState(() {});
+        },
+      ),
+    );
+  }
+}
