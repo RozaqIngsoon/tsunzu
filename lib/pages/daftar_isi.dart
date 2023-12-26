cc97fb823b7fe95fb469c1ce624a47aaa1a091eb
RST <email.ingsoon@gmail.com>
2023-12-26 22:50:02 +0700

    update daftar isi

--- lib/pages/daftar_isi.dart
+++ lib/pages/daftar_isi.dart
@@ -9,22 +9,25 @@ class DaftarIsi extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Widget isi(
-        {required String no,
-        required String judul,
-        required String halaman,
-        required int lompatke}) {
+    var lebar = MediaQuery
+        .of(context)
+        .size
+        .width;
+    Widget isi({
+      required String judul,
+      required String halaman,
+      required int lompatke}) {
       return ListTile(
-        leading: Text(
-          ' $no.',
-          style: TextStyle(color: Colors.white, fontSize: MediaQuery.of(context).size.width / 20,backgroundColor: Color(0xff1d2031)),
-        ),
-        title: Text(judul, style: TextStyle(color: Colors.white, fontSize: MediaQuery.of(context).size.width / 20)),
+        title: Text(
+            judul, style: TextStyle(color: Colors.white, fontSize: lebar / 20)),
+        subtitle: Divider(color: Colors.white),
         trailing:
-            Text(halaman, style: TextStyle(color: Colors.white, fontSize: MediaQuery.of(context).size.width / 20)),
+        Text(halaman, style: TextStyle(color: Colors.white, fontSize: lebar / 20)),
         onTap: () {
-          controllerPageFlip.currentState?.goToPage(lompatke); //menggunakan index page
+          controllerPageFlip.currentState?.goToPage(
+              lompatke); //menggunakan index page
         },
+        visualDensity: VisualDensity(horizontal: 0, vertical: -4),
       );
     }
 
@@ -43,32 +46,33 @@ class DaftarIsi extends StatelessWidget {
             ),
             Expanded(
                 child: Container(
-              padding:
+                  padding:
                   const EdgeInsets.only(top: 20, bottom: 8, right: 8),
-              child: ListView(
-                children: [
-                  Text('  Daftar Isi',
-                      style: GoogleFonts.merienda(
-                        textStyle: TextStyle(color: Colors.yellow, fontSize: MediaQuery.of(context).size.width / 18,fontWeight: FontWeight.bold,letterSpacing: 1.0),
-                      )),
-                  isi(
-                      no: '1',
-                      judul: 'Perencanaan',
-                      halaman: '0${pages.indexOf(bab1_cover)}',
-                      lompatke: pages.indexOf(bab1_cover),),
-                  isi(
-                    no: '2',
-                    judul: 'Peperangan',
-                    halaman: '0${pages.indexOf(bab2_cover)}',
-                    lompatke: pages.indexOf(bab2_cover),),
-                  isi(
-                    no: '3',
-                    judul: 'Strategi',
-                    halaman: '${pages.indexOf(bab3_cover)}',
-                    lompatke: pages.indexOf(bab3_cover),),
-                ],
-              ),
-            ))
+                  child: ListView(
+                    children: [
+                      Text('  Daftar Isi',
+                          style: GoogleFonts.merienda(
+                            textStyle: TextStyle(
+                                color: Colors.yellow,
+                                fontSize: lebar / 18,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 1.0),
+                          )),
+                      isi(
+                        judul: 'Perencanaan',
+                        halaman: '0${pages.indexOf(bab1_cover)}',
+                        lompatke: pages.indexOf(bab1_cover),),
+                      isi(
+                        judul: 'Peperangan',
+                        halaman: '0${pages.indexOf(bab2_cover)}',
+                        lompatke: pages.indexOf(bab2_cover),),
+                      isi(
+                        judul: 'Strategi',
+                        halaman: '${pages.indexOf(bab3_cover)}',
+                        lompatke: pages.indexOf(bab3_cover),),
+                    ],
+                  ),
+                ))
           ],
         ),
       ),
