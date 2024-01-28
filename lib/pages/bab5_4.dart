23ab84093608cd32ae57a709321654ea5cf17b13
RST <email.ingsoon@gmail.com>
2024-01-29 02:43:12 +0700

    revisi bab 5 slesai

--- lib/pages/bab5_4.dart
+++ lib/pages/bab5_4.dart
@@ -40,5 +40,5 @@ const String teks = '''
 ### Energi bisa dibandingkan dengan membengkokkan busur panah; keputusan, dengan melepaskan pelatuknya.
 ### Di tengah kerusuhan dan kegemparan pertempuran, meskipun mungkin terlihat kacau, sebenarnya tidak ada kekacauan sama sekali. Di tengah kebingungan dan kekacauan, formasimu mungkin terlihat tanpa kepala atau ekor, namun itu akan tetap tahan terhadap kekalahan.
 ### Ketika kita membuat kekacauan yang disimulasikan, kita mengasumsikan adanya disiplin yang sempurna; ketika kita menunjukkan ketakutan yang disimulasikan, kita mengasumsikan adanya keberanian; dan ketika kita menampakkan kelemahan yang disimulasikan, kita sebenarnya sedang menunjukkan kekuatan.
-### Menyembunyikan ketertiban di balik tirai kekacauan hanyalah masalah membagi-bagi; menyembunyikan keberanian di balik tampilan ketakutan mengasumsikan adanya energi yang terpendam; menyamarkan kekuatan dengan kelemahan dapat dicapai melalui taktik disposisi*.
+### Menyembunyikan ketertiban di balik tirai kekacauan hanyalah masalah
 ''';
--- lib/pages/bab5_6.dart
+++ lib/pages/bab5_6.dart
@@ -39,9 +39,8 @@ class BabLima6 extends StatelessWidget {
 }
 
 const String teks = '''
-### terpilih, dia menanti-nantikan musuh.
-### Prajurit yang cerdik memperhatikan efek dari energi yang digabungkan, dan tidak memerlukan terlalu banyak dari individu. Oleh karena itu, dia memiliki kemampuan untuk memilih orang-orang yang tepat dan memanfaatkan energi yang digabungkan.
-### Ketika dia memanfaatkan energi yang digabungkan, para prajuritnya menjadi seperti kayu gelinding atau batu. Karena adalah sifat dari kayu atau batu untuk tetap diam di tanah datar, dan bergerak saat berada di lereng; jika berbentuk empat sudut, akan berhenti, tetapi jika berbentuk bulat, akan bergulir ke bawah.
+### memanfaatkan energi yang digabungkan.
+### Ketika dia memanfaatkan energi yang digabungkan, para prajuritnya menjadi seperti kayu gelinding atau batu. Seperti halnya sifat kayu atau batu untuk tetap diam di tanah datar dan bergerak saat berada di lereng; jika berbentuk empat sudut, akan berhenti, tetapi jika berbentuk bulat, akan bergulir ke bawah.
 ### Dengan demikian, energi yang dikembangkan oleh para pejuang yang baik adalah seperti momentum dari batu bulat yang bergulir turun dari gunung yang tinggi ribuan kaki. 
 
 ''';
