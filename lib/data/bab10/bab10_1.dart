9a9160440d6dcd9e6befdc41473098b603fe8144
RST <email.ingsoon@gmail.com>
2024-03-20 21:37:50 +0700

    ganti total 75%

--- lib/data/bab10/bab10_1.dart
+++ lib/data/bab10/bab10_1.dart
@@ -1,9 +1,15 @@
 Map bab10_1 = {
    'bab10_1' :'''
-###  maka pasukan akan terhindar dari penyakit, dan ini akan membawa kemenangan.
-### Saat kamu sampai di sebuah bukit atau tebing, kuasai sisi yang terkena sinar matahari, dengan kemiringan ke belakang sebelah kananmu. Dengan cara ini, kamu akan memberikan keuntungan bagi para prajuritmu dan sekaligus memanfaatkan keunggulan alami dari tanah tersebut.
-### Ketika, akibat hujan deras di daerah hulu, sungai yang ingin kamu lintasi mengalami peluberan dan berbusa, kamu harus menunggu hingga air surut.
-### Daerah yang memiliki tebing curam dengan aliran deras di antara mereka, lubang alami yang dalam, tempat-tempat sempit, semak-semak yang berantakan, rawa-rawa, dan celah-celah harus segera ditinggalkan dan tidak mendekat.
-### Sementara kita menjauhi tempat-tempat
+## Bab 10 : MEDAN PERTEMPURAN
+### Kita dapat membedakan enam jenis medan, yaitu: 
+1)	### Medan yang dapat diakses.
+2)	### Medan yang rumit.
+3)	### Medan yang memungkinkan penundaan.
+4)	### Medan jalur sempit. 
+5)	### Medan ketinggian curam. 
+6)	### Medan yang jauh dari musuh.
+### Wilayah yang dapat dilewati dengan bebas oleh kedua belah pihak disebut “Medan yang dapat diakses”.
+### Dalam wilayah yang memiliki karakteristik seperti ini, penting untuk menduduki posisi yang tinggi di depan musuh dan terkena sinar matahari. Selain itu, jagalah jalur pasokanmu dengan baik. Dengan cara ini, kamu akan memiliki keunggulan yang baik dalam pertempuran.
+
 '''
 };
